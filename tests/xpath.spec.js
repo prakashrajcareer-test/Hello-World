f4871d89502df2f2028ddd3c735c3ecf2baf8d76
prakashrajcareer-test <prakashraj.career@gmail.com>
2025-07-29 14:23:24 +0530

    go out

--- tests/xpath.spec.js
+++ tests/xpath.spec.js
@@ -1,4 +1,5 @@
 import {test}from '@playwright/test'
+import { Session } from 'node:inspector/promises'
 
 test ('find X path',async({page})=>{
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
@@ -7,6 +8,44 @@ test ('find X path',async({page})=>{
     await page.locator ('//button[type="submitt"]').click()
     })
 
+    312W7E1965308
+    ------------
+
+    import {test}from '@playwright/test'
+import { Session } from 'node:inspector/promises'
+
+test ('find X path',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.locator('//input[placeholder="username"]').fill('Admin')
+    await page.locator ('//input[placeholder="password"]').fill('admin123')
+    await page.locator ('//button[type="submitt"]').click()
+    })
+
+    312W7E1965308
+    ------------
+
+    import {test}from '@playwright/test'
+import { Session } from 'node:inspector/promises'
+
+test ('find X path',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.locator('//input[placeholder="username"]').fill('Admin')
+    await page.locator ('//input[placeholder="password"]').fill('admin123')
+    await page.locator ('//button[type="submitt"]').click()
+    })
+
+    312W7E1965308
+    ------------
+
+    import {test}from '@playwright/test'
+import { Session } from 'node:inspector/promises'
+
+test ('find X path',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.locator('//input[placeholder="username"]').fill('Admin')
+    await page.locator ('//input[placeholder="password"]').fill('admin123')
+    await page.locator ('//button[type="submitt"]').click()
+    })
    
     test('Another way',async({page})=>{
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
@@ -29,3 +68,18 @@ test ('find X path',async({page})=>{
     await page.locator (':inventory.item').fliter({hastext:'sauce labs fleece jacket'}).getByText('ADD TO CART').click()
         })
 
+
+      Session -200
+    ----------------  
+      
+import('static drop down',async({page})=>{
+await page.goto('https://rahulshettyacademy.com/automationpractice/')
+const select_dropdown=page.locator('#dropdown_class_example')
+await select_dropdown.selectOption('option1')
+await select_dropdown.selectOption('option2')
+await select_dropdown.selectOption('option3')
+await select_dropdown.selectOption('Index:1')
+await select_dropdown.selectOption('Index:2')
+await select_dropdown.selectOption('Index:3')
+})
+  
